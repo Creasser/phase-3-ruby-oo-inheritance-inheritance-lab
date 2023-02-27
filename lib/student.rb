9442e99a4cb704cf8656d05bf7743b4f7e8be6b1
Blake Creasser <blakecreasser@aggienetwork.com>
2023-02-26 22:06:00 -0600

    commit tree 940604ed93eecc1ecf141adaec0eb55262b8de88

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative = 'lib/user'
+
+class Student < User
+    
+    attr_accessor :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn string
+        self.knowledge << string
+        self.knowledge
+    end
 
 end
